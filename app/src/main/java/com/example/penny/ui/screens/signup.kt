e0cf863f39856de92fd86b6905272afe707e801f
timovibes <timothyator744@gmail.com>
2026-05-09 20:19:02 +0300

    test: add temporary signup logo preview

--- app/src/main/java/com/example/penny/ui/screens/signup.kt
+++ app/src/main/java/com/example/penny/ui/screens/signup.kt
@@ -4,22 +4,33 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.penny.R
 
 @Composable
-fun SignUp(modifier: Modifier = Modifier){
+fun SignUp(modifier: Modifier){
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally)
     {
         Image(
             painter = painterResource(R.drawable.logo),
-            contentDescription = "logo"
+            contentDescription = "logo",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(16.dp))
         )
     }
 }
